ed134b6bd90193338dc4c9d4f09faee06ecee416
JKThanassi <josephkamingthanassi@gmail.com>
2020-12-09 08:28:18 -0500

    Finished day 3

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 import qualified Days.Day01 as D1 (solution1, solution2)
 import qualified Days.Day02 as D2 (solution, solution2)
+import qualified Days.Day03 as D3 (solution)
 
 main :: IO ()
 main = do 
-    result <- D2.solution "src/inputs/day02.txt"
-    result2 <- D2.solution2 "src/inputs/day02.txt"
+    result <- D3.solution "src/inputs/day03.txt"
+--    result2 <- D2.solution2 "src/inputs/day02.txt"
     print result  
-    print result2  
+--    print result2  
 
--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,18 +1,24 @@
-module Days.Day03 where 
+module Days.Day03 (solution) where 
 
 import Parser (fileToList)
 import qualified Data.Vector as V
+
+data SledState = SledState {dRow :: Int, dCol :: Int, cRow :: Int, cCol :: Int} 
 
 solution :: String -> IO Int
 solution path = do
       lines <- fileToList path
       let lov = map V.fromList lines
       let vov = V.fromList lov
-      return 3
+      let los = [SledState 1 1 0 0, SledState 1 3 0 0, SledState 1 5 0 0, SledState 1 7 0 0, SledState 2 1 0 0]
+      return $ product $ map (\a -> findTrees a vov 0) los
 
 getIdx :: Int -> Int -> Int
 getIdx idx len = idx `mod` len
 
-findTrees :: Int -> Int -> Int -> Int -> V.Vector (V.Vector Char)-> Int -> Int
-findTrees _ _ _ _ [] treeAcc = treeAcc
-findTrees moveD moveR row col vecLst treeAcc =  3
+findTrees :: SledState -> V.Vector (V.Vector Char) -> Int -> Int
+findTrees state vec treeAcc 
+  | cRow state >= V.length vec = treeAcc
+  | vec V.! cRow state V.! cCol state == '#' = findTrees (state {cRow=dRow state + cRow state, cCol=getIdx (cCol state + dCol state) $ V.length $ vec V.! 0}) vec (1 + treeAcc)
+  | otherwise = findTrees (state {cRow=dRow state + cRow state, cCol=getIdx (cCol state + dCol state) $ V.length $ vec V.! 0}) vec treeAcc
+
